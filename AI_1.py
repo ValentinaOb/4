3d6d674c92fcabb2dd8747d1de53e4135d7b2e75
user <user@WIN-MHGL1PRC8MH>
2025-09-12 18:11:26 +0300

    1

--- AI_1.py
+++ AI_1.py
@@ -20,11 +20,12 @@ D=3 #random members #10
 population = []
 cross=[]
 mut=[]
-rand=[]
+r_d=[]
 graphic_points=[]
 graphic_crossover=[]
 graphic_mutation=[]
 grahp_elements=[]
+graphic_r_d=[]
 
 indiv=[]
 f,x_prev,y_prev=0,0,0
@@ -38,7 +39,6 @@ ax=None
 
 def is_inside(point, polygon):
     return polygon.contains_point(point)
-
 
 def individual ():
     '''xc=random.randrange(0,999)
@@ -69,22 +69,47 @@ def individual ():
 
             return [xc,xd,yc,yd,f]
 
-def crossover (f,m):
-    cross.clear()
+def crossover (fa,m):
+    global f
     r_point=random.randrange(0,4)
     print('\n\nf, m, r_point ',f,m, r_point)
 
-    ind1=f[:r_point] + m[r_point:]
-    ind2=m[:r_point] + f[r_point:]
+    ind1=fa[:r_point] + m[r_point:]
+    ind2=m[:r_point] + fa[r_point:]
+
+    x_prev=float(str(fa[0])+'.'+str(fa[1]))
+    y_prev=float(str(fa[2])+'.'+str(fa[3]))
+    x=float(str(ind1[0])+'.'+str(ind1[1]))
+    y=float(str(ind1[2])+'.'+str(ind1[3]))
+    
+    f+=a/(((x-x_prev)**2)+((y-y_prev)**2)+1)
+    ind1[4]=f
+
+
+    x_prev=float(str(m[0])+'.'+str(m[1]))
+    y_prev=float(str(m[2])+'.'+str(m[3]))
+    x=float(str(ind2[0])+'.'+str(ind2[1]))
+    y=float(str(ind2[2])+'.'+str(ind2[3]))
+    
+    f+=a/(((x-x_prev)**2)+((y-y_prev)**2)+1)
+    ind2[4]=f
+
     print('\nind ',ind1,ind2)
     return ind1, ind2
 
-def mutation (ind):
-    mut.clear()
+def mutation (ind):    
+    global f
     r_point=random.randrange(0,4)
     new_random=random.randint(0,999)
     new_ind=ind[:r_point] +[new_random]+ ind[r_point+1:] #???
     
+    x_prev=float(str(ind[0])+'.'+str(ind[1]))
+    y_prev=float(str(ind[2])+'.'+str(ind[3]))
+    x=float(str(new_ind[0])+'.'+str(new_ind[1]))
+    y=float(str(new_ind[2])+'.'+str(new_ind[3]))
+    
+    f+=a/(((x-x_prev)**2)+((y-y_prev)**2)+1)
+    new_ind[4]=f
 
     print('\n\nf, r_point ',ind, r_point)
     print('\nnew_ind ',new_ind)
@@ -99,6 +124,7 @@ def selection ():
     grahp_elements.sort(key=lambda x: x[2], reverse=True)
     
     print("\nPopulation ", population)
+    population.clear()
     
     for i in range(P):
         xc,xd = int(grahp_elements[i][0]), int(str((round(grahp_elements[i][0] - int(grahp_elements[i][0]),3))).replace("0.",""))
@@ -108,10 +134,21 @@ def selection ():
     print("\nNew population ", population)
 
 def algor(btn):
-    global population, r_f, r_m
+    global population, r_f, r_m,r_d,grahp_elements,graphic_crossover,graphic_mutation,graphic_points,graphic_r_d
 
     if btn==0:
         population = [individual() for i in range(P)]
+    else:
+        print('\n\n\nHere\n\n')
+        mut.clear()
+        cross.clear()
+        r_d.clear()
+        grahp_elements.clear()
+        graphic_crossover.clear()
+        graphic_mutation.clear()
+        graphic_points.clear()
+        graphic_r_d.clear()
+        
     print('\nPopulation ',population)
 
     #4
@@ -138,22 +175,26 @@ def algor(btn):
     r_d = [individual() for i in range(D)]
     print('\nD ',r_d)
 
-    prepare_for_graphic()
-
-    #7
-    selection()
-
+    #prepare_for_graphic()
 
     #
     prepare_for_graphic()
     grahp()
 
+
+    #7
+    selection()
+
+
+    
+    
+
     #
 
 def prepare_for_graphic(): #connect
-    global population, cross, mut
-    global graphic_points, graphic_crossover, graphic_mutation, grahp_elements
-    graphic_points, graphic_crossover, graphic_mutation, grahp_elements=[],[],[],[]
+    global population, cross, mut, r_d
+    global graphic_points, graphic_crossover, graphic_mutation, grahp_elements, graphic_r_d
+    graphic_points, graphic_crossover, graphic_mutation, grahp_elements, graphic_r_d =[],[],[],[],[]
     
 
     for i in range(len(population)):
@@ -174,14 +215,19 @@ def prepare_for_graphic(): #connect
         f=mut[i][4]
         graphic_mutation.append([x,y,f])
 
-    grahp_elements=graphic_points+graphic_crossover+graphic_mutation
+    for i in range(len(r_d)):
+        x=float(str(r_d[i][0])+'.'+str(r_d[i][1]))
+        y=float(str(r_d[i][2])+'.'+str(r_d[i][3]))
+        f=r_d[i][4]
+        graphic_r_d.append([x,y,f])
+
+    grahp_elements=graphic_points+graphic_crossover+graphic_mutation+graphic_r_d
     
 def grahp():
     global graphic_points, graphic_crossover, graphic_mutation, grahp_elements, ax
 
     ax = plt.axes(projection='3d')
-    #ax = plt.axes(projection='3d')
-
+    
     # Polygon
     for i in range(len(polygon_3d)):
         plt.scatter(polygon_3d[i][0],polygon_3d[i][1],0)
@@ -196,9 +242,15 @@ def grahp():
         ax.plot([graphic_crossover[i][0], graphic_crossover[i][0]], [graphic_crossover[i][1], graphic_crossover[i][1]], [0, graphic_crossover[i][2]],color='lightblue')
 
     for i in range(len(graphic_mutation)):
-        ax.scatter(graphic_mutation[i][0], graphic_mutation[i][1], graphic_mutation[i][2], color='yellow')
+        ax.scatter(graphic_mutation[i][0], graphic_mutation[i][1], graphic_mutation[i][2], color='orange')
         ax.plot([graphic_mutation[i][0], graphic_mutation[i][0]], [graphic_mutation[i][1], graphic_mutation[i][1]], [0, graphic_mutation[i][2]],color='lightblue')
 
+    #
+    for i in range(len(graphic_r_d)):
+        ax.scatter(graphic_r_d[i][0], graphic_r_d[i][1], graphic_r_d[i][2], color='black')
+        ax.plot([graphic_r_d[i][0], graphic_r_d[i][0]], [graphic_r_d[i][1], graphic_r_d[i][1]], [0, graphic_r_d[i][2]],color='lightblue')
+
+    #
     x_coords, y_coords = zip(*polygon_3d)
     ax.plot(x_coords, y_coords,color ='pink')
     
@@ -209,8 +261,7 @@ def grahp():
         if max< grahp_elements[i][2]:
             max=grahp_elements[i][2]
             max_z_index=i
-    #ax.scatter(graphic_points[max_z_index][0], graphic_points[max_z_index][1], graphic_points[max_z_index][2], color='red')
-    ax.text(graphic_points[max_z_index][0], graphic_points[max_z_index][1], graphic_points[max_z_index][2],  'm', size=10, color='r') 
+    ax.text(grahp_elements[max_z_index][0], grahp_elements[max_z_index][1], grahp_elements[max_z_index][2],  'm', size=10, color='r') 
     '''
     for i in range(len(graphic_points)):
         if max< graphic_points[i][2]:
@@ -233,29 +284,29 @@ def grahp():
     
     ax.set_title('3D Scatter Plot')
     
-
-    #return ax
-    #plt.show()
-
 def update_canvas():    
+    
     fields_gr['canvas'].draw()
+    
+    for item in fields_gr['table'].get_children():
+        fields_gr['table'].delete(item)
     k=0
     for i in grahp_elements:
         #for x, y, f in zip(i[0], i[1],i[2]):
         if k==0:
             fields_gr['table'].insert("", "end", values=('', 'Population',''), tags=('population'))
-        elif k==P:
+        elif k==P: #and k<P+C
             fields_gr['table'].insert("", "end", values=('', 'Crossover',''), tags=('crossover'))
-        elif k==P+C:
+        elif k==P+2*C: #and k<P+C+M
             fields_gr['table'].insert("", "end", values=('', 'Mutation',''), tags=('mutation'))
-        elif k==P+C+M:
+        elif k==P+2*C+M:
             fields_gr['table'].insert("", "end", values=('', 'D',''), tags=('d'))
         fields_gr['table'].insert("", "end", values=(i[0], i[1],i[2]))
         k+=1
 
     fields_gr['table'].tag_configure('population', background='lightgreen')
-    fields_gr['table'].tag_configure('crossover', background='brown')
-    fields_gr['table'].tag_configure('mutation', background='lightpink')
+    fields_gr['table'].tag_configure('crossover', background='blue')
+    fields_gr['table'].tag_configure('mutation', background='orange')
     fields_gr['table'].tag_configure('d', background='grey')
 
 fields={}
@@ -268,12 +319,23 @@ def pre_start ():
     M=int(fields['mutation'].get())
     D=int(fields['d'].get())'''
 
+    global population, cross, mut, r_d
+    population, cross, mut, r_d = [],[],[],[]
+    global graphic_points, graphic_crossover,graphic_mutation,grahp_elements,graphic_r_d
+    graphic_points, graphic_crossover,graphic_mutation,grahp_elements,graphic_r_d=[],[],[],[],[]
+
     algor(0)
+    grahp()
     update_canvas()
     #main()
 
-def re_pop():
+def re_pop():    
+    global cross, mut, r_d
+    cross, mut, r_d = [],[],[]
+    global graphic_points, graphic_crossover,graphic_mutation,grahp_elements,graphic_r_d
+    graphic_points, graphic_crossover,graphic_mutation,grahp_elements,graphic_r_d=[],[],[],[],[]
     algor(1)
+    grahp()
     update_canvas()
 
 
